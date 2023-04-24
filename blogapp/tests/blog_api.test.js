4216b15a9b019d7bb5ecaf4e990590cb2e7af7c6
FU <>
2023-04-24 21:05:34 +0200

    ex 4.8 adding the first api test

--- blogapp/tests/blog_api.test.js
+++ blogapp/tests/blog_api.test.js
@@ -7,18 +7,39 @@ const api = supertest(app)
 
 const initialBlogs = [
     {
-        title: 'testBlog 1',
-        author: 'me',
-        url: '123www..dfa',
-        likes: 10 
+        _id: '5a422a851b54a676234d17f7',
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: '5a422b3a1b54a676234d17f9',
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 12,
+        __v: 0
     }
 ]
 
 beforeEach(async () => {
 
     await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
-    await blogObject.save()
+    let blogObjects = initialBlogs.map(blog => new Blog(blog))
+    
+    const promiseArray = blogObjects.map(blog => blog.save())
+
+    await Promise.all(promiseArray)
 
 })
 
@@ -29,12 +50,18 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test('the correct amount of blogs is returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(initialBlogs.length)
+})
+
 test('check the contents of the blogs', async () => {
     const response = await api.get('/api/blogs')
 
     const title = response.body[0].title
 
-    expect(title).toContain('testBlog 1')
+    expect(title).toContain(initialBlogs[0].title)
 
 })
 
